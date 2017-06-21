dd8971e42680a2f997436d358c6e69db22143516
Manmay Kumar <mkmmanmay@gmail.com>
2017-06-21 17:19:02 +0530

    Added some more line of code as the learning progressed.

--- thirdTest.py
+++ thirdTest.py
@@ -8,8 +8,11 @@ class Human():
     def speak_name(self):
         print("My name is ", self.name)
 
-    def speak(text):
+    def speak(self, text):
         print(text)
+
+    def perform_math_task(self, math_operation, *args):
+        print("{} performed a math operation, and the result was: {}".format(self.name, math_operation(*args)))
 
 
 will = Human("William", "Male")
@@ -20,9 +23,16 @@ print(will.gender)
 will.speak_name()
 will.speak("I love programming.")
 
+
 #  The above wouldn't work if you remove 'self' from the method "speak()".
 #  Self is an implied (implicit) argument which has to be given to a method.
 #  If you remove self as an argument from the method, and try running above code
 #  you will get an error like below.
 #  will.speak("I love programming.")
 #  TypeError: speak() takes 1 positional argument but 2 were given
+
+def add(a, b, c, d):
+    return a + b + c + d
+ryan = Human("Ryan Stevens", "Male")
+ryan.perform_math_task(add, 34, 6, 4, 10)
+
